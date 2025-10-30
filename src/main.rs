f703cd7b837ed0fbdaaa2c0a72e4013e6509167a
yugisaki <franekstrocen@gmail.com>
2025-10-30 20:07:14 +0000

    side panel

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@ use eframe::egui;
 
 struct Minesweeper {
     mines: i32,
+    height: i32,
+    width: i32,
 }
 
 impl eframe::App for Minesweeper {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("config").show(ctx, |ui| {
+            ui.vertical_centered_justified(|ui| {
+                ui.add(egui::Slider::new(&mut self.mines, 0..=self.height*self.width -2).text("Mines"));
+                ui.add(egui::Slider::new(&mut self.height, 0..=200).text("Height"));
+                ui.add(egui::Slider::new(&mut self.width, 0..=200).text("Width"));
+                if ui.button("Start Game").clicked() {
+                    // generate the map
+                }
+            });
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("test");
         });
@@ -22,6 +34,12 @@ fn main() {
     eframe::run_native(
         "Minesweeper",
         options,
-        Box::new(|_cc| Ok(Box::new(Minesweeper { mines: 24 }))),
+        Box::new(|_cc| {
+            Ok(Box::new(Minesweeper {
+                mines: 24,
+                height: 24,
+                width: 24,
+            }))
+        }),
     );
 }
